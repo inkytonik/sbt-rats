9f9f31d0d585c245c8319e3b0cdf9650a3840008
Tony Sloane <inkytonik@gmail.com>
2012-06-27 16:58:35 +0200

    Add support for transforming generated Java code, add rats-use-scala-lists setting

--- src/SBTRatsPlugin.scala
+++ src/SBTRatsPlugin.scala
@@ -10,6 +10,8 @@ import xtc.parser.Rats
 
 object SBTRatsPlugin extends Plugin {
 
+    import scala.util.matching.Regex
+
     /**
      * The file that contains the main Rats! module. We assume that all
      * other Rats! files are brought in by this one.
@@ -19,21 +21,33 @@ object SBTRatsPlugin extends Plugin {
     )
 
     /**
+     * If true, assume that the Rats!-generated parser is to be used with
+     * Scala and use Scala lists for repeated constructs. Otherwise, use
+     * the default Rats! pair-based lists.
+     */
+    val ratsUseScalaLists = SettingKey[Boolean] (
+        "rats-use-scala-lists", "Use Scala lists instead of Rats! pair-based lists"
+    )
+
+    /**
      * Run the Rats! parser generator if any of the .rats files in the source have
      * changed or the output doesn't exist.
      */
     def runRats =
-        (sourceManaged in Compile, streams, ratsMainModule, scalaSource, cacheDirectory) map {
-            (smdir, str, main, srcdir, cache) => {
+        (ratsMainModule, ratsUseScalaLists,
+         target, sourceManaged in Compile, streams, scalaSource, cacheDirectory) map {
+            (main, useScalaLists,
+             tgtDir, smDir, str, srcDir, cache) => {
 
                 val cachedFun =
                     FileFunction.cached (cache / "sbt-rats", FilesInfo.lastModified,
                                          FilesInfo.exists) {
                         (in: Set[File]) =>
-                            runRatsImpl (smdir, str, main, srcdir)
+                            runRatsImpl (main, useScalaLists,
+                                         tgtDir, smDir, str)
                     }
 
-                val inputFiles = (srcdir ** "*.rats").get.toSet
+                val inputFiles = (srcDir ** "*.rats").get.toSet
                 cachedFun (inputFiles).toSeq
 
             }
@@ -44,24 +58,91 @@ object SBTRatsPlugin extends Plugin {
      * sbt-rats directory under the target's sourceManaged directory, which is
      * created if it doesn't exist.
      */
-    def runRatsImpl (smdir : File, str : TaskStreams, main : File, srcdir : File) : Set[File] = {
-        val outdir = smdir / "sbt-rats"
+    def runRatsImpl (main : File, useScalaLists : Boolean,
+                     tgtDir: File, smDir : File, str : TaskStreams) : Set[File] = {
+        val genDir = tgtDir / "sbt-rats"
+        IO.createDirectory (genDir)
+        val outDir = smDir / "sbt-rats"
+        IO.createDirectory (outDir)
         val mainPath = main.absolutePath
-        IO.createDirectory (outdir)
-        str.log.info ("Running Rats! on %s".format (mainPath))
+        val mainDir = main.getParentFile
+        str.log.info ("Running Rats! on %s, output to %s".format (mainPath, genDir))
         val rats = new RatsRunner ()
         rats.run (Array ("-silent", "-no-exit",
-                         "-in", srcdir.absolutePath,
-                         "-out", outdir.absolutePath,
+                         "-in", mainDir.absolutePath,
+                         "-out", genDir.absolutePath,
                          mainPath))
         if (rats.getRuntime.seenError) {
-            sys.error ("Rats failed")
+            sys.error ("Rats! failed")
             Set.empty
         } else {
-            val genfiles = (outdir ** "*.java").get.toSet
-            str.log.info ("Rats! generated %s".format (genfiles.mkString))
-            genfiles
+            val genFiles = (genDir ** "*.java").get.toSet
+            genFiles.size match {
+                case 1 =>
+                    val genFile = genFiles.head
+                    str.log.info ("Rats! generated %s".format (genFile))
+                    val outFile = outDir / genFile.name
+                    if (useScalaLists) {
+                        str.log.info ("Rats! transforming %s for Scala into %s".format (
+                                          genFile, outFile))
+                        transformForScala (genFile, outFile, useScalaLists)
+                    } else {
+                        str.log.info ("Rats! copying %s to %s".format (genFile, outFile))
+                        IO.copyFile (genFile, outFile, true)
+                    }
+                    Set (outFile)
+                case 0 =>
+                    sys.error ("Rats! didn't generate any files")
+                    Set.empty
+                case _ =>
+                    sys.error ("Rats! didn't generated more than one file: %s".format (
+                                    genFiles.mkString))
+                    Set.empty
+            }
         }
+    }
+
+    /**
+     * Make the indicated replacements in the given string, returning the result.
+     */
+    def makeReplacements (contents : String, replacements : List[(Regex,String)]) : String =
+        replacements.foldLeft (contents) {
+            case (s, r) =>
+                r._1.replaceAllIn (s, r._2)
+        }
+
+    /**
+     * Transform the generated file into the output file as per the flag parameters.
+     *  - useScalaLists: replace xtc pairs with Scala lists
+     */
+    def transformForScala (genFile : File, outFile : File, useScalaLists : Boolean) {
+
+        def transformPairsToLists (contents : String) : String = {
+            val pairsToLists =
+                List (
+                    "import xtc.util.Pair;".r ->
+                        """import scala.collection.immutable.List;
+                          |import scala.collection.immutable.\$colon\$colon;""".stripMargin,
+                    "Pair.empty".r ->
+                        "List.empty",
+                    """new Pair(<[^>]+>)\(([^,)]+)\)""".r -> 
+                        """new \$colon\$colon$1($2, List.empty)""",
+                    """new Pair(<[^>]+>)""".r -> 
+                        """new \$colon\$colon$1""",
+                    "Pair(<[^>]+>)".r ->
+                        "List$1"
+                )
+            makeReplacements (contents, pairsToLists)
+        }
+
+        val contents = IO.read (genFile)
+        val contents1 = 
+            if (useScalaLists)
+                transformPairsToLists (contents)
+            else
+                contents
+        IO.write (outFile, contents1)                
+
     }
 
     /**
@@ -74,11 +155,13 @@ object SBTRatsPlugin extends Plugin {
 
         sourceGenerators in Compile <+= runRats,
 
-        compileOrder in Compile := CompileOrder.JavaThenScala,
+        // compileOrder in Compile := CompileOrder.JavaThenScala,
 
         libraryDependencies ++= Seq (
             "rats" % "rats" % "2.3.1" from "http://cs.nyu.edu/rgrimm/xtc/rats.jar"
-        )
+        ),
+
+        ratsUseScalaLists := false
 
     )
 
