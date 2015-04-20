a224da7814be37f9c11c95b9013d097c1d180c8e
Tony Sloane <inkytonik@gmail.com>
2015-04-21 08:53:51 +1000

    Replace Scala code that escaped into binary error code with Java equivalent

--- src/Translator.scala
+++ src/Translator.scala
@@ -104,7 +104,7 @@ class Translator (analyser : Analyser) extends PrettyPrinter {
                 |        if (c != -1) {
                 |            buf.append ((char)c);
                 |        } else {
-                |            return new ParseError (s"expected $n bytes but EOF found", base + i);
+                |            return new ParseError (String.format ("expected %d bytes but EOF found", n), base + i);
                 |        }
                 |    }
                 |    return new SemanticValue (buf.toString (), base + n);
